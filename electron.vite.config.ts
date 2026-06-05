8636ca72a1db8a0638b02ef205276864ae815a0c
GFrancV <gfrancv@hotmail.com>
2026-06-05 16:46:18 +0100

    fix: update paths in configuration files to reflect new directory structure

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -2,27 +2,24 @@ import { resolve } from 'path'
 
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import { defineConfig } from 'electron-vite'
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()],
     resolve: {
       alias: {
-        '@main': resolve('src/main'),
-      },
-    },
+        '@main': resolve('src/main')
+      }
+    }
   },
-  preload: {
-    plugins: [externalizeDepsPlugin()],
-  },
+  preload: {},
   renderer: {
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
-        '@': resolve('src/renderer'),
-        '@renderer': resolve('src/renderer'),
-      },
-    },
-  },
+        '@': resolve('src/renderer/src'),
+        '@renderer': resolve('src/renderer/src')
+      }
+    }
+  }
 })
