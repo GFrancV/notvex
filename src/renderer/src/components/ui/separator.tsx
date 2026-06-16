13be7df53ecfbb25fa71110749cfc9438694fbb1
GFrancV <gfrancv@hotmail.com>
2026-06-16 11:46:00 +0100

    fix: remove unused 'use client' directive and adjust JSX import type in Separator component

--- src/renderer/src/components/ui/separator.tsx
+++ src/renderer/src/components/ui/separator.tsx
@@ -1,7 +1,4 @@
-'use client'
-
 import { Separator as SeparatorPrimitive } from 'radix-ui'
-import type { JSX } from 'react'
 
 import { cn } from '@/lib/utils'
 
@@ -10,7 +7,7 @@ function Separator({
   orientation = 'horizontal',
   decorative = true,
   ...props
-}: React.ComponentProps<typeof SeparatorPrimitive.Root>): JSX.Element {
+}: React.ComponentProps<typeof SeparatorPrimitive.Root>): React.JSX.Element {
   return (
     <SeparatorPrimitive.Root
       data-slot="separator"
